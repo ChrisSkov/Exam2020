828a3734c646b0c322c6fe747037e8afee066f10
Chris <Stofferdyr@gmail.com>
2020-01-13 11:07:44 +0100

    DTO classes in progress

---
+++ src/main/java/DTO/DriverDTO.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import entities.Driver;
+import entities.Truck;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author Chris
+ */
+public class DriverDTO {
+
+    private String name;
+    private int id;
+    private List<TruckDTO> truckList = new ArrayList();
+
+    public DriverDTO()
+    {
+    }
+
+    public DriverDTO(Driver driver)
+    {
+        this.id = driver.getId();
+        this.name = driver.getName();
+        for (Truck t : driver.getTrucks())
+        {
+            truckList.add(new TruckDTO(t));
+        }
+
+    }
+
+    public void addTruckDTO(TruckDTO t)
+    {
+        this.truckList.add(t);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public List<TruckDTO> getTruckList()
+    {
+        return truckList;
+    }
+
+    public void setTruckList(List<TruckDTO> truckList)
+    {
+        this.truckList = truckList;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.name);
+        hash = 73 * hash + this.id;
+        hash = 73 * hash + Objects.hashCode(this.truckList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final DriverDTO other = (DriverDTO) obj;
+        if (this.id != other.id)
+        {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.truckList, other.truckList))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "DriverDTO{" + "name=" + name + ", id=" + id + ", truckList=" + truckList + '}';
+    }
+
+}
---
+++ src/main/java/DTO/TruckDTO.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import entities.Driver;
+import entities.Truck;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author Chris
+ */
+public class TruckDTO {
+
+    private Integer id;
+    private String name, capacity;
+
+    private List<DriverDTO> driverList = new ArrayList();
+
+    TruckDTO(Truck truck)
+    {
+        this.id = truck.getId();
+        this.name = truck.getName();
+        this.capacity = truck.getCapacity();
+        for (Driver d : truck.getDrivers())
+        {
+            driverList.add(new DriverDTO(d));
+        }
+    }
+
+    public TruckDTO()
+    {
+    }
+
+    public void addDriverDTO(DriverDTO d)
+    {
+        this.driverList.add(d);
+    }
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId(Integer id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getCapacity()
+    {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity)
+    {
+        this.capacity = capacity;
+    }
+
+    public List<DriverDTO> getDriverList()
+    {
+        return driverList;
+    }
+
+    public void setDriverList(List<DriverDTO> driverList)
+    {
+        this.driverList = driverList;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + Objects.hashCode(this.capacity);
+        hash = 97 * hash + Objects.hashCode(this.driverList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final TruckDTO other = (TruckDTO) obj;
+        if (!Objects.equals(this.name, other.name))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.capacity, other.capacity))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id))
+        {
+            return false;
+        }
+        if (!Objects.equals(this.driverList, other.driverList))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "TruckDTO{" + "id=" + id + ", name=" + name + ", capacity=" + capacity + ", driverList=" + driverList + '}';
+    }
+
+}
